f3f591b3fb672deb5c0c02218a87b456aaa811dd
Aidan Fitzgerald <duckjr159@gmail.com>
2016-06-09 16:03:31 -0400

    Barycentric

---
+++ linalg.py
@@ -0,0 +1,39 @@
+
+def add(a, b):
+    return [a[i] + b[i] for i in range(len(a))]
+
+def neg(a):
+    return [-x for x in a]
+
+def subtract(a, b):
+    return add(a, neg(b))
+
+def dot_product(a, b):
+    if len(a) == len(b):
+        return sum([a[i] * b[i] for i in range(len(a))])
+    else:
+        raise ValueError("Dot product defined only for vectors of same dimension")
+
+def cross_product(a, b):
+    if len(a) == len(b) == 3:
+        return [a[(i+1) % 3] * b[(i+2) % 3] - a[(i+2) % 3] * b[(i+1) % 3] for i in range(3)]
+    else:
+        # Technically, cross product is also defined for 7-D vectors, but we're not working in 7-D are we?
+        raise ValueError("Cross product defined only for vectors of dimension 3")
+
+def scalar_triple_product(a, b, c):
+    return dot_product(a, cross_product(b, c))
+
+def magnitude(a):
+    return sqrt(dot_product(a, a))
+
+def normalize(a):
+    mag = magnitude(a)
+    return [x/mag for x in a]
+
+'''
+Returns the projection of a onto b
+'''
+def project(a, b):
+    f = dot(a, b) / dot(b, b)
+    return [x * f for x in b]
