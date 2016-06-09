5d66710666664cdc1e5cfc303609e15841f93228
Aidan Fitzgerald <duckjr159@gmail.com>
2016-06-09 16:02:34 -0400

    Starting on barycentric coordinates.  Learned an algorithm for projecting points onto lines and planes

--- draw3d.py
+++ draw3d.py
@@ -1,5 +1,6 @@
 from draw import *
 from math import ceil, sqrt
+from linalg import *
 
 class BoxVertices:
     def __init__(self, x, y, z, width, height, depth):
@@ -247,26 +248,3 @@ def surface_normal(matrix, index):
     #return [c / 2 for c in cross_product(a, b)]
     # But to keep it simple, we'll return the cross product
     return cross_product(a, b)
-
-def normalize(a):
-    mag = magnitude(a)
-    return [x/mag for x in a]
-
-def magnitude(a):
-    return sqrt(sum([x*x for x in a]))
-
-def cross_product(a, b):
-    if len(a) == len(b) == 3:
-        return [a[(i+1) % 3] * b[(i+2) % 3] - a[(i+2) % 3] * b[(i+1) % 3] for i in range(3)]
-    else:
-        # Technically, cross product is also defined for 7-D vectors, but we're not working in 7-D are we?
-        raise ValueError("Cross product defined only for vectors of dimension 3")
-
-def dot_product(a, b):
-    if len(a) == len(b):
-        return sum([a[i] * b[i] for i in range(len(a))])
-    else:
-        raise ValueError("Dot product defined only for vectors of same dimension")
-
-def scalar_triple_product(a, b, c):
-    return dot_product(a, cross_product(b, c))
