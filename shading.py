768e9c1e23a444c8e0810fde5507b2e415350f31
Aidan Fitzgerald <duckjr159@gmail.com>
2016-06-13 11:22:47 -0400

    start shading

--- shading.py
+++ shading.py
@@ -1,45 +1,71 @@
-from linalg import *
+from draw import *
+
+I_a = [40, 12, 35]
+I_d = [200, 90, 60]
+I_s = [0, 80, 40]
+
+I = vector_sum(I_a, I_d, I_s)
 
 
 class Light:
-    channels = ['r', 'g', 'b']
+    channels = [RED, GREEN, BLUE]
     refl_types = ['ambient', 'diffuse', 'specular']
+
+    FLAT = 0
+    GOURAUD = 1
+    PHONG = 2
 
     def __init__(self):
         self.constants = {
-            ('ambient', 'r'): 0.0,
-            ('ambient', 'g'): 0.0,
-            ('ambient', 'b'): 0.0,
-            ('diffuse', 'r'): 0.0,
-            ('diffuse', 'g'): 0.0,
-            ('diffuse', 'b'): 0.0,
-            ('specular', 'r'): 0.0,
-            ('specular', 'g'): 0.0,
-            ('specular', 'b'): 0.0,
+            'ambient': [0, 0, 0],
+            'diffuse': [0, 0, 0],
+            'specular': [0, 0, 0]
         }
-        self.lights = {}
+        self.point_lights = []
+        self.ambient_light = [0, 0, 0]
 
     def get_constants(self):
         return self.constants
 
     def get_constant(self, refl_type, color):
-        return self.constants[refl_type, color]
+        return self.constants[refl_type][color]
 
     def set_constant(self, refl_type, color, value):
-        self.constants[refl_type, color] = value
+        self.constants[refl_type][color] = value
 
     def normalize_constants(self):
         for color in channels:
             S = sum(map(lambda refl: self.get_constant(refl, color), refl_types))
-            if S:
+            if S > 1:
                 for refl_type in refl_types:
-                    self.constants[refl_type, color] /= S
+                    self.constants[refl_type][color] /= S
 
     def add_light(self, point, color):
-        if point is not None:
-            self.lights[point] = color
-        else:
-            raise ValueError('Point cannot be None')
+        self.point_lights.append( (point, color) )
+
+    def get_ambient_light(self):
+        return self.ambient_light
 
     def set_ambient_light(self, color):
-        self.lights[None] = color
+        self.ambient_light = color
+
+    def get_point_light(self, i):
+        return self.point_lights[i]
+
+    def set_point_light(self, i, point, color):
+        self.point_lights[i] = (point, color)
+
+    def remove_point_light(self, i):
+        return self.point_lights.pop(i)
+
+    def shade(self, matrix, index, mode):
+        # Get ambient light
+        I = self.ambient_light[:]
+        for color in channels:
+            # Multiply by ambient reflection constants
+            I[color] *= self.get_constant('ambient', color)
+
+            p = centroid(matrix, index)
+            # Point lights
+            for point, colour in self.point_lights:
+                incident_vector = normalize(subtract(point, p))
