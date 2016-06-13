b688b315f6dfa95bbf13a6b1da21e1a09c6ed507
Dalton Wu <dalton.z.wu@gmail.com>
2016-06-13 17:46:37 -0400

    Z-buffering does not work:clock530:

--- test.py
+++ test.py
@@ -2,9 +2,18 @@ from display import *
 from draw import *
 
 screen = new_screen()
+
 p1 = [100, 100, 100, 1]
 p2 = [150, 100, 105, 1]
 p3 = [150, 200, 100, 1]
-triangle = [p1, p2, p3]
-draw_triangle(triangle, 0, screen, [0, 255, 0], fill=True)
+p4 = p1[:2] + [0, 1]
+p5 = p2[:2] + [0, 1]
+p6 = p3[:2] + [0, 1]
+
+tri1 = [p1, p2, p3]
+tri2 = [p4, p5, p6]
+
+draw_triangle(tri1, 0, screen, [0, 255, 0], fill=True)
+draw_triangle(tri2, 0, screen, [0, 128, 255], fill=True)
+
 save_ppm(screen, "test.ppm.gz")
